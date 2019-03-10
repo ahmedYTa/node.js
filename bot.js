acbaa6b64108270d8b4856dd4fb38fea7a532af9
ahmedYTa <48285249+ahmedYTa@users.noreply.github.com>
2019-03-11 01:18:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
