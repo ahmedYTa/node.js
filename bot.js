0b191738e4ca732e829f96a362f301cfcca70283
ahmedYTa <48285249+ahmedYTa@users.noreply.github.com>
2019-03-11 01:15:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,26 +10,72 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
-client.on('message', async msg => {
-    if(msg.content.startsWith(prefix + 'bc')) {
-      let roleW = msg.mentions.roles.first();
-      let args2 = msg.content.split(" ").slice(2).join(" ");
-       if(!msg.guild.members.get(msg.author.id).hasPermission('ADMINISTRATOR')) return msg.channel.send('Required Administrator Permission') 
-       let role = msg.guild.roles.find(`name`, roleW.name);
-       if(!role) return msg.reply(`Could't find \`${roleW.name}\` role.`).then( msgs => msgs.delete(3000)); 
-       let nomsg = 0;
-         msg.channel.send(`**- [ :mailbox_closed:  :: ${nomsg} ] ・عدد الرسائل المرسلة**`).then(msgs => {
-         role.members.forEach(m =>{
-        m.send(args2.replace('[user]', m).replace('[server]', m.guild.name).replace('[sender]', msg.author.username)).then( () =>{
-          nomsg++;
-                  if(!msgs) return;
-                  msgs.edit(`**- [ :mailbox_closed:  :: ${nomsg} ] ・عدد الرسائل المرسلة**`);
+client.on('message',async message => {
+
+  if(message.author.bot || message.channel.type === `bc') return;
+
+  let args = message.content.split(' ');
+
+  if(args[0] === -bc) {
+
+    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر');
+
+    if(!args[1]) return message.channel.send('- يجب عليك كتابة الرسالة بعد الأمر');
+
+ 
+
+    let msgCount = 0;
+
+    let errorCount = 0;
+
+    let successCount = 0;
+
+    message.channel.send(- [ bookmark :: ${msgCount} ] ・عدد الرسائل المرسلة\n- [ inbox_tray :: ${successCount} ] ・عدد الرسائل المستلمة\n- [ outbox_tray :: ${errorCount} ]・عدد الرسائل الغير مستلمة).then(msg => {
+
+      message.guild.members.forEach(g => {
+
+        g.send(args.slice(1).join(' ')).then(() => {
+
+          successCount++;
+
+          msgCount++;
+
+          msg.edit(- [ bookmark :: ${msgCount} ] ・عدد الرسائل المرسلة\n- [ inbox_tray :: ${successCount} ] ・عدد الرسائل المستلمة\n- [ outbox_tray :: ${errorCount} ]・عدد الرسائل الغير مستلمة);
+
         }).catch(e => {
-          nomsg++;
-                  if(!msgs) return;
-          msgs.edit(`**- [ :mailbox_closed:  :: ${nomsg} ] ・عدد الرسائل المرسلة**`);
+
+          errorCount++;
+
+          msgCount++;
+
+          msg.edit(- [ bookmark :: ${msgCount} ] ・عدد الرسائل المرسلة\n- [ inbox_tray :: ${successCount} ] ・عدد الرسائل المستلمة\n- [ outbox_tray :: ${errorCount} ]・عدد الرسائل الغير مستلمة);
+
         });
-        }); 
+
       });
-      }})
+
+    });
+
+  }
+
+});
+client.on('message' , message => {
+if(message.content === 'help') {
+  var EsTeKnAN = new Discord.RichEmbed()
+  .setColor('RANDOM')
+message.author.send(وصف عن البوت
+
+─════════════ {✯اوامر البوت✯} ════════════─
+❧ ^bc ➺ برودكاست ب امبيد وبدون
+❧ ^Link ➺ رابط انفايت للسيرفر
+❧ ^clear ➺ مسح الشات
+❧ ^server ➺ لعرض معلومات السيرفر
+❧ ^marry ➺ لعبة الزواج
+❧ ^kf ➺ لعبة كف
+❧ ^mc ➺ قفل الشات
+❧ ^new ➺ لانشاء تذكرة
+─════════════ {✯ By Dream ✯} ════════════─
+);
+}
+})
 client.login(process.env.BOT_TOKEN);
